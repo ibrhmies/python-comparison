8dc46cf5e1daf4256137d901505253ec82cd9d2c
ibrhmies <02220201044@ogr.inonu.edu.tr>
2022-11-09 23:09:08 +0300

    comparison
    python'da karşılaştırma operatörleri

---
+++ comparison.py
@@ -0,0 +1,16 @@
+a, b, c, d = 5, 5, 20, 10
+
+sonuc = (a == b) #a, b ye eşit mi sorusunun komut olarak karşılığı (==) cevap=true 
+
+sonuc = (a != b) #-> a,b ye eşit değil mi sorusunun komut olarak karşılığı (!=) cevap =false
+
+username = 'ibrhmies'
+
+sonuc = (username == 'ibrhmies') #->true
+sonuc = (username != 'ibrmies') #->true burda sorduğumuz soru eşit değil mi sorusudur.
+
+sonuc = (c > a) # burda c a dan büyük mü diye sorduk.
+
+sonuc = (a > b) # a, b den büyük mü diye soruyoruz cevap false çünkü eşitler
+sonuc = (a >= b) # a, b den büyük yada eşit mi diye soruyoruz cevap true 
+print(sonuc)
